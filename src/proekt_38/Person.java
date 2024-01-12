06ea68b175d9faa5913c3496f09ed9955a9f8aa0
iki55 <aleksovska.isidora@gmail.com>
2024-01-13 00:14:45 +0100

    first program

---
+++ src/proekt_38/Person.java
@@ -0,0 +1,16 @@
+package proekt_38;
+
+public class Person {
+	private String name;
+	private int age;
+	public Person(String name) {
+		this.name=name;
+	}
+	public void setAge(int age) {
+		this.age=age;
+	}
+	public int getAge() {
+		return age;
+	}
+
+}
